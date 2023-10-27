becba4fbade543765de3a9a732156d41d65e4d4c
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-10-27 19:16:58 +0500

    исправила ошибку

--- homework-1/main.py
+++ homework-1/main.py
@@ -9,10 +9,10 @@ if __name__=='__main__':
             csv_list = csv.DictReader(file)
             return list(csv_list)
 
-
     csv_list_orders = csv_open('north_data/orders_data.csv')
     csv_list_employees = csv_open('north_data/employees_data.csv')
     csv_list_customers = csv_open('north_data/customers_data.csv')
+
 
     conn = psycopg2.connect(
         host='localhost',
@@ -20,34 +20,28 @@ if __name__=='__main__':
         user='postgres',
         password='12345'
     )
+
     with conn.cursor() as curs:
 
         for employe in csv_list_employees:
             curs.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (
-            employe['employee_id'], employe['first_name'], employe['last_name'], employe['title'],
-            employe['birth_date'], employe['notes']))
+                employe['employee_id'], employe['first_name'], employe['last_name'], employe['title'],
+                employe['birth_date'], employe['notes']))
             curs.execute("SELECT * FROM employees")
             data_employe = curs.fetchall()
 
         for customer in csv_list_customers:
-            curs.execute('INSERT INTO employees VALUES (%s, %s, %s)', (
-            customer['customer_id'], customer['company_name'],
-            customer['contact_name']))
+            curs.execute('INSERT INTO customers VALUES (%s, %s, %s)', (
+                customer['customer_id'], customer['company_name'],
+                customer['contact_name']))
             curs.execute("SELECT * FROM employees")
             data_customer = curs.fetchall()
 
         for order in csv_list_orders:
-            curs.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s)', (
+            curs.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', (
                 order['order_id'], order['customer_id'], order['employee_id'], order['order_date'],
                 order['ship_city']))
             curs.execute("SELECT * FROM employees")
             data_order = curs.fetchall()
 
-    # conn.commit
-    # conn.close()
-        for el in data_order:
-            print(el)  # вывести все значения таблицы
-    # data1=curs.fetchone # одну строчку
-    # data2=curs.fetchmany # указать скока строк
-    # curs.close # закрыть
-
+    conn.close()
