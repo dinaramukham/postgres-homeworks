4f1c289cf972bc61310fc1841695dd6b3f0c8ef0
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-10-27 19:15:08 +0500

    первое дз, создала таблицы заполнила их данными

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,24 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name text,
+	last_name text,
+	title text,
+	birth_date date,
+	notes text
+);
+CREATE TABLE orders
+(
+	order_id text PRIMARY KEY,
+	customer_id text REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date,
+	ship_city text
+);
+CREATE TABLE customers
+(
+	customer_id text PRIMARY KEY,
+	company_name text,
+	contact_name text
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,53 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+if __name__=='__main__':
+    import csv
+    import psycopg2
+
+
+    def csv_open(name_file):  # 'north_data/customers_data.csv'  north_data/employees_data.csv  north_data/orders_data.csv
+        with open(name_file) as file:
+            csv_list = csv.DictReader(file)
+            return list(csv_list)
+
+
+    csv_list_orders = csv_open('north_data/orders_data.csv')
+    csv_list_employees = csv_open('north_data/employees_data.csv')
+    csv_list_customers = csv_open('north_data/customers_data.csv')
+
+    conn = psycopg2.connect(
+        host='localhost',
+        database='north',
+        user='postgres',
+        password='12345'
+    )
+    with conn.cursor() as curs:
+
+        for employe in csv_list_employees:
+            curs.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', (
+            employe['employee_id'], employe['first_name'], employe['last_name'], employe['title'],
+            employe['birth_date'], employe['notes']))
+            curs.execute("SELECT * FROM employees")
+            data_employe = curs.fetchall()
+
+        for customer in csv_list_customers:
+            curs.execute('INSERT INTO employees VALUES (%s, %s, %s)', (
+            customer['customer_id'], customer['company_name'],
+            customer['contact_name']))
+            curs.execute("SELECT * FROM employees")
+            data_customer = curs.fetchall()
+
+        for order in csv_list_orders:
+            curs.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s)', (
+                order['order_id'], order['customer_id'], order['employee_id'], order['order_date'],
+                order['ship_city']))
+            curs.execute("SELECT * FROM employees")
+            data_order = curs.fetchall()
+
+    # conn.commit
+    # conn.close()
+        for el in data_order:
+            print(el)  # вывести все значения таблицы
+    # data1=curs.fetchone # одну строчку
+    # data2=curs.fetchmany # указать скока строк
+    # curs.close # закрыть
+
